841dbdd4ca8d33277f901ada0248a0320749305f
Anonymouseyy <79242981+Anonymouseyy@users.noreply.github.com>
2022-07-03 14:11:43 -0700

    socket/network beginnings

--- client.py
+++ client.py
@@ -19,6 +19,27 @@ smallFont = pg.font.Font('Roboto-Black.ttf', 14)
 mediumFont = pg.font.Font('Roboto-Black.ttf', 28)
 largeFont = pg.font.Font('Roboto-Black.ttf', 40)
 
+# Socket Variables
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
+SERVER = '192.168.0.16'
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send_str(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
 
 while True:
     event_list = pg.event.get()
